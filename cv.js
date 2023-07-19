0898337a172bdff883cb6ff83bb98c26b1a0c88e
nkezuwimye <92544423+nkezuwimye@users.noreply.github.com>
2023-07-19 11:28:20 +0200

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,23 @@
+ 
+        .body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+        }
+
+       . h1 {
+            text-align: center;
+        }
+
+        .section {
+            margin-bottom: 20px;
+        }
+
+        .section h2 {
+            margin-bottom: 10px;
+        }
+
+        .section p {
+            margin: 0;
+        }
+    
